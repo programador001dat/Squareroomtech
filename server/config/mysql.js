112973cf6415f2f0ed68c020e3b9bcd99ea83853
programador001dat <155966937+programador001dat@users.noreply.github.com>
2025-09-17 02:35:36 -0300

    Update mysql.js

--- server/config/mysql.js
+++ server/config/mysql.js
@@ -1,24 +1,23 @@
 // Conexão entre MySQL e Express.
-
-const mysql = require("mysql"); // módulo do MySQL.
+const mysql = require("mysql"); // Importando o modulo para dentro do codigo
 
 // criando um objeto com as configurações para conectar ao MySQL.
 const conn = mysql.createConnection({
     host: "localhost",
-    user: "admin",
-    password: "01000001",
-    database: "admin"
+    user: "xxxxx",
+    password: "xxxxx",
+    database: "xxxxx"
 });
 
 
-// Conectando ao Banco se não houver Tratamento de Erro. 
+// Conectando ao Banco e configurando um Tratamento de Erro. 
 conn.connect((error) => {
     if (error)
     {
         console.log(`Error ao conectar com o MySQL ${error}`);
     }else
     {
-        console.log("Conexão com Sucesso MySQL\n");
+        console.log("Conexão com Sucesso MySQL\n"); // Se não der erro nas credenciais, ele retorna.
     }
 });
 
